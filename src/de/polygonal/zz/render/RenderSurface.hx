c41378c31b1acba4b9cb2b2d6d21d541ff7a8011
Michael Baczynski <michaelbaczynski@gmail.com>
2013-05-30 14:45:43 +0200

    RenderSurface is now always resizable

--- src/de/polygonal/zz/render/RenderSurface.hx
+++ src/de/polygonal/zz/render/RenderSurface.hx
@@ -29,9 +29,9 @@
  */
 package de.polygonal.zz.render;
 
-import de.polygonal.core.fmt.Sprintf;
 import de.polygonal.core.math.Vec2;
 import de.polygonal.motor.geom.primitive.AABB2;
+import de.polygonal.core.math.Mathematics;
 
 using Reflect;
 
@@ -62,8 +62,7 @@ typedef SurfaceArgs =
 class RenderSurface
 {
 	inline static var HARDWARE  = 0x01;
-	inline static var RESIZABLE = 0x02;
-	inline static var READY     = 0x04;
+	inline static var READY     = 0x02;
 	
 	/**
 	 * The width of this surface in pixels.
@@ -80,6 +79,9 @@ class RenderSurface
 	 */
 	public static var bound(default, null):AABB2 = null;
 	
+	/**
+	 * The mouse position relative to the top-left corner of the surface.
+	 */
 	public static var mouse(get_mouse, never):Vec2;
 	static function get_mouse():Vec2
 	{
@@ -99,6 +101,84 @@ class RenderSurface
 	 */
 	public static var onResize:Int->Int->Void = null;
 	
+	public static function computeBoundShowAll(contentW:Int, contentH:Int, cropThreshold = 0., allowNonUniformScale = false):{x:Float, y:Float, scaleX:Float, scaleY:Float}
+	{
+		var sW = RenderSurface.width;
+		var sH = RenderSurface.height;
+		var sX = sW / contentW;
+		var sY = sH / contentH;
+		var x = 0.;
+		var y = 0.;
+		
+		if (sX <= sY)
+		{
+			//landscape
+			y = Math.round((sH - (contentH * sX)) / 2);
+			
+			var letterbox = sH - (sX * contentH);
+			L.d('letterbox: ' + Math.ceil(letterbox));
+			
+			//allow some cropping (left/right) if letterbox covers less than 2 percent of the screen height
+			if (letterbox / sH < cropThreshold)
+			{
+				sX = sY;
+				sY = sX;
+				y = 0;
+				x = Math.round((sW - (contentW * sX)) / 2);
+			}
+			else
+			{
+				sY = sX;
+				if (allowNonUniformScale)
+				{
+					//in case letterbox is used, allow some distortion so new height snaps to a full pixel
+					var t = Math.round(contentH * sX);
+					if (!M.isEven(t)) t--;
+					
+					sY = t / contentH;
+					y = (sH - t) >> 1;
+				}
+			}
+		}
+		else
+		{
+			//portrait
+			x = Math.round((sW - (contentW * sY)) / 2);
+			
+			var pillarbox = sW - (sY * contentW);
+			
+			L.d('pillarbox: ' + Math.ceil(pillarbox));
+			
+			//allow some cropping (top/bottom) if pillarbox covers less than 2 percent of the screen width
+			if (pillarbox / sW < cropThreshold)
+			{
+				sY = sX;
+				sX = sY;
+				x = 0;
+				y = Math.round((sH - (contentH * sY)) / 2);
+			}
+			else
+			{
+				sX = sY;
+				if (allowNonUniformScale)
+				{
+					//in case pillarbox is used, allow some distortion so new width snaps to a full pixel
+					var t = Math.round(contentW * sY);
+					if (!M.isEven(t)) t--;
+					
+					sX = t / contentW;
+					x = (sW - t) >> 1;
+				}
+			}
+		}
+		
+		L.d('content scale: x=${sX} y=${sY}');
+		L.d('content size: ${contentW*sX}/${contentH*sY} px');
+		L.d('content offset: ${x}/${y} px');
+		
+		return {x: x, y: y, scaleX: sX, scaleY: sY};
+	}
+	
 	static var _mouse:Vec2;
 	static var _flags:Int = 0;
 	static var _onCreate:Void->Void = null;
@@ -107,10 +187,10 @@ class RenderSurface
 	public static var stage(default, null):flash.display.Stage = null;
 	public static var root(default, null):flash.display.Sprite = null;
 	#if flash11
-	public static var stage3D:flash.display.Stage3D = null;
+	public static var stage3d:flash.display.Stage3D = null;
 	public static var numDeviceLost = -1;
 	#if flash11_4
-	public static var profile = flash.display3D.Context3DProfile.BASELINE;
+	public static var profile = flash.display3D.Context3DProfile.BASELINE_CONSTRAINED;
 	#end
 	#end
 	#end
@@ -138,19 +218,11 @@ class RenderSurface
 		return (_flags & HARDWARE) > 0;
 	}
 	
-	/**
-	 * Returns true if this surface is resizable.
-	 */
-	public static function isResizable():Bool
-	{
-		return (_flags & RESIZABLE) > 0;
-	}
-	
 	public static function create(onCreate:Void->Void, onResize:Int->Int->Void = null, args:SurfaceArgs = null)
 	{
 		_onCreate = function()
 		{
-			trace(Sprintf.format('surface created (%03dx%03dpx)', [width, height]));
+			L.i('surface initialized (${width}x${height})');
 			_flags |= READY;
 			onCreate();
 		}
@@ -163,9 +235,6 @@ class RenderSurface
 		
 		_mouse = new Vec2();
 		
-		if (args.hasField('resizable') && args.resizable)
-			_flags |= RESIZABLE;
-		
 		#if flash
 		if (args != null)
 		{
@@ -177,8 +246,14 @@ class RenderSurface
 				profile = args.field('profile');
 			#end
 		}
+		
+		L.d('context 3d profile is $profile');
+		
 		initDisplayList();
-		#elseif nme
+		#elseif cpp
+		#if nme
+		initDisplayList();
+		#else
 		var fps = 60;
 		var color = 0;
 		width = 640;
@@ -202,13 +277,14 @@ class RenderSurface
 		try
 		{
 			nme.Lib.create(function() { initDisplayList(); }, width, height, fps, color,
-				((isHardware() ? 1 : 0) * nme.Lib.HARDWARE) | (isResizable() ? 1 : 0) * nme.Lib.RESIZABLE);
+				((isHardware() ? 1 : 0) * nme.Lib.HARDWARE) | 1 * nme.Lib.RESIZABLE);
 		}
 		catch (error:Dynamic)
 		{
 			//nme.Lib.create already called
 			initDisplayList();
 		}
+		#end
 		#elseif js
 		_tmp = new Vec2();
 		_jq = new js.JQuery(js.Lib.window).ready(
@@ -253,51 +329,65 @@ class RenderSurface
 	
 	public static function destroy():Void
 	{
+		#if flash
+		//remove all display objects in a bottom(=leaf)->up fashion
+		var a:Array<flash.display.DisplayObject> = [];
+		function rec(x:flash.display.DisplayObject)
+		{
+			if (Std.is(x, flash.display.DisplayObjectContainer))
+			{
+				var o:flash.display.DisplayObjectContainer = cast x;
+				for (i in 0...o.numChildren) rec(o.getChildAt(i));
+			}
+			a.push(x);
+		}
+		rec(flash.Lib.current.stage);
+		for (i in a) i.parent.removeChild(i);
+
+
 		#if flash11
-		de.polygonal.zz.render.module.flash.util.DisplayListUtil.removeChildren(stage);
 		if (isHardware())
 		{
 			try 
 			{
-				stage3D.removeEventListener(flash.events.Event.CONTEXT3D_CREATE, function(e) _onCreate());
+				stage3d.removeEventListener(flash.events.Event.CONTEXT3D_CREATE, function(e) _onCreate());
 			}
 			catch(error:Dynamic) {}
 		}
+		#end
 		#end
 		
 		_flags &= ~READY;
 	}
 	
 	#if (flash || nme)
-	static function initDisplayList()
+	static function initDisplayList():Void
 	{
 		stage = flash.Lib.current.stage;
 		root = new flash.display.Sprite();
+		root.mouseEnabled = false;
 		root.name = 'root';
 		root.addEventListener(flash.events.Event.ADDED_TO_STAGE, onAddedToStage);
 		flash.Lib.current.addChild(root);
 	}
 	
-	static function onAddedToStage(e)
+	static function onAddedToStage(e):Void
 	{
 		cast(e.target, flash.display.Sprite).removeEventListener(flash.events.Event.ADDED_TO_STAGE, onAddedToStage);
 		
 		bound.maxX = width = stage.stageWidth;
 		bound.maxY = height = stage.stageHeight;
 		
-		if (isResizable())
-		{
-			#if flash
-			stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
-			stage.align = flash.display.StageAlign.TOP_LEFT;
-			stage.addEventListener(flash.events.Event.RESIZE, onStageResize);
-			#end
-		}
+		#if (flash || nme)
+		stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		stage.align = flash.display.StageAlign.TOP_LEFT;
+		stage.addEventListener(flash.events.Event.RESIZE, onStageResize);
+		#end
 		
 		if (isHardware())
 		{
 			#if flash11
-			initStage3D();
+			initStage3d();
 			return;
 			#end
 		}
@@ -307,34 +397,34 @@ class RenderSurface
 	#end
 	
 	#if flash11
-	static function initStage3D()
+	static function initStage3d():Void
 	{
 		stage = flash.Lib.current.stage;
-		stage3D = flash.Lib.current.stage.stage3Ds[0];
-		stage3D.addEventListener(flash.events.Event.CONTEXT3D_CREATE, onContext3DCreate);
+		stage3d = flash.Lib.current.stage.stage3Ds[0];
+		stage3d.addEventListener(flash.events.Event.CONTEXT3D_CREATE, onContext3DCreate);
 		
 		#if flash11_4
-		stage3D.requestContext3D(cast flash.display3D.Context3DRenderMode.AUTO, profile);
+		stage3d.requestContext3D(cast flash.display3D.Context3DRenderMode.AUTO, profile);
 		#else
-		stage3D.requestContext3D(cast flash.display3D.Context3DRenderMode.AUTO);
+		stage3d.requestContext3D(cast flash.display3D.Context3DRenderMode.AUTO);
 		#end
 	}
 	
-	static function onContext3DCreate(_)
+	static function onContext3DCreate(_):Void
 	{
 		if (++numDeviceLost == 0)
 			_onCreate();
 	}
 	#end
 	
-	static function onStageResize(e)
+	static function onStageResize(_):Void
 	{
-		#if flash
+		#if (flash || nme)
 		bound.maxX = width = stage.stageWidth;
 		bound.maxY = height = stage.stageHeight;
 		#end
 		
-		trace(Sprintf.format('surface resized (%03dx%03dpx)', [width, height]));
+		L.i('surface resized to ${width}x${height}');
 		if (onResize != null) onResize(width, height);
 	}
 }
